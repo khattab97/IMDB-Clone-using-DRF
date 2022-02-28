34f77a3c0feb2877530b76563f2868cb0347508a
Milk Steak <a.khatttab97@gmail.com>
2022-03-01 01:46:53 +0200

    Adding Review model in models, serializers, admin

--- watchlist/admin.py
+++ watchlist/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from watchlist.models import WatchList, StreamPlatform
+from watchlist.models import WatchList, StreamPlatform, Review
 # Register your models here.
 
 
 admin.site.register(WatchList)
 admin.site.register(StreamPlatform)
+admin.site.register(Review)
