2b7edca76daaf19a84279c18915cf2cb7192ddb0
Milk Steak <a.khatttab97@gmail.com>
2022-03-03 02:48:45 +0200

    Created RegistrationSerializers and Registration function view and adding to urls

--- userauth/api/urls.py
+++ userauth/api/urls.py
@@ -1,7 +1,10 @@
 from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
+    path('register/', views.registration_view, name='register'),
+
 ]
