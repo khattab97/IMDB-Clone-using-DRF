2c4060f4793b3d4fc7e1ff88526892e90e446934
Milk Steak <a.khatttab97@gmail.com>
2022-03-03 03:23:59 +0200

    Adding log view and its url

--- userauth/api/views.py
+++ userauth/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework import status
 from .serializers import RegistrationSerializer
 from userauth import models
 
@@ -24,3 +25,11 @@ def registration_view(request):
             data = serializer.errors
 
         return Response(data)
+
+
+@api_view(['POST'])
+def logout_view(request):
+
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
